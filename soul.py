544df8b9991e69f49d90e2b44a85572e9021f919
cadebreakeravinash <avinashbgminew@gmail.com>
2024-07-28 00:51:04 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = ':AAFBzV22zX5WJ9nsfRRIxk625KCzrApynUc'
+TOKEN = ':AAHP70T_iJ0AV5Sfx_0rGz7xNFYbAryWEB8'
 MONGO_URI = 'mongodb+srv://piroop:piroop@piro.hexrg9w.mongodb.net/?retryWrites=true&w=majority&appName=piro&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1002225117171
-CHANNEL_ID = -1002225117171
-error_channel_id = -1002225117171
+FORWARD_CHANNEL_ID = -6682104026
+CHANNEL_ID = -6682104026
+error_channel_id = -6682104026
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
